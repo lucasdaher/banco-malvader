88583456a852686278f5f52fc5988912018725b4
lucasdaher <contato@lucasdaher.com>
2024-06-14 14:51:34 -0300

    Documentando funções utilizadas na classe principal.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
+// Requisitando a biblioteca contendo as funcionalidades do banco.
 #include "malvader.h"
 
 int main()
 {
+  // Definindo linguagem padrão do programa para o mesmo do sistema operacional.
   setlocale(LC_ALL, "");
+
+  // Envia o menu principal assim que o programa roda.
   enviarMenuPrincipal();
 
-  getch();
+  // Retorna 0 por padrão.
   return 0;
 }
